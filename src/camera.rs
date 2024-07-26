8b62cfa99fff4c06365f9ad47e492c2582c809c5
Stephan Roslen <stephan@roslen.de>
2024-07-26 06:17:25 +0200

    Chapter 5

---
+++ src/camera.rs
@@ -0,0 +1,39 @@
+use crate::prelude::*;
+
+pub struct Camera {
+    pub left_x: i32,
+    pub right_x: i32,
+    pub top_y: i32,
+    pub bottom_y: i32,
+}
+
+impl Camera {
+    pub fn new(player_position: Point) -> Self {
+        let mut result = Self {
+            left_x: 0,
+            right_x: 0,
+            top_y: 0,
+            bottom_y: 0,
+        };
+        result.on_player_move(player_position);
+        result
+    }
+
+    fn trim_value(lower: i32, val: i32, upper: i32) -> i32 {
+        use std::cmp::{max, min};
+
+        min(max(lower, val), upper)
+    }
+
+    pub fn on_player_move(&mut self, player_position: Point) {
+        let adapted_player_position = Point::new(
+            Self::trim_value(DISPLAY_WIDTH/2, player_position.x, SCREEN_WIDTH - DISPLAY_WIDTH/2),
+            Self::trim_value(DISPLAY_HEIGHT/2, player_position.y, SCREEN_HEIGHT - DISPLAY_HEIGHT/2),
+        );
+
+        self.left_x = adapted_player_position.x - DISPLAY_WIDTH / 2;
+        self.right_x = adapted_player_position.x + DISPLAY_WIDTH / 2;
+        self.top_y = adapted_player_position.y - DISPLAY_HEIGHT / 2;
+        self.bottom_y = adapted_player_position.y + DISPLAY_HEIGHT / 2;
+    }
+}
---
+++ src/map.rs
@@ -0,0 +1,73 @@
+use crate::prelude::*;
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+
+    pub fn in_bounds(point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        Self::in_bounds(point) && self.tiles[Self::map_idx(point)] == TileType::Floor
+    }
+
+    pub fn map_idx(point: Point) -> usize {
+        ((point.y * SCREEN_WIDTH) + point.x) as usize
+    }
+
+    pub fn try_idx(point: Point) -> Option<usize> {
+        if !Self::in_bounds(point) {
+            None
+        } else {
+            Some(Self::map_idx(point))
+        }
+    }
+
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+        ctx.set_active_console(0);
+        for y in camera.top_y..camera.bottom_y {
+            for x in camera.left_x..camera.right_x {
+                let point = Point::new(x, y);
+                if Self::in_bounds(point) {
+                    let idx = Self::map_idx(point);
+                    match self.tiles[idx] {
+                        TileType::Floor => {
+                            ctx.set(
+                                x - camera.left_x,
+                                y - camera.top_y,
+                                WHITE,
+                                BLACK,
+                                to_cp437('.'),
+                            );
+                        }
+                        TileType::Wall => {
+                            ctx.set(
+                                x - camera.left_x,
+                                y - camera.top_y,
+                                WHITE,
+                                BLACK,
+                                to_cp437('#'),
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
